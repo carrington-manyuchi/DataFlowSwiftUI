f467732bc5e9f47d6e4a127667575b201860726a
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-15 09:01:29 +0200

    Initial Commit

---
+++ DataFlowSwiftUI/DataFlowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowSwiftUIApp.swift
+//  DataFlowSwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2024/12/15.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
